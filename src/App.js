faea08899fd29a935f5b2e928a460813f9cdc110
marioquiroga <mquiroga@dosalcubo.com>
2020-06-24 02:39:25 -0300

    primary styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import './App.css';
+import styled from '@emotion/styled'
+
+import imagen from './cryptomonedas.png'
+
+/* Styled Components */
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+
+  @media (min-width:992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: white;
+  text-align: left;
+  font-weight: 800;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Contenedor>
+      <div>
+          <Imagen src={imagen} alt='cryptomonedas-logo' />
+      </div>
+      <div>
+        <Heading>Cotiza Criptomonedas al instante</Heading>
+      </div>
+    </Contenedor>
   );
 }
 
